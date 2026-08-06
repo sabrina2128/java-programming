cf83c4bc7385c7809a9e312cfebcf7e863e06340
sabrina2128 <t717069@gmail.com>
2026-08-06 11:30:08 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,14 @@
+public class Logical {
+  public static void main(String[]args)
+  {
+     int a,b,c,d;
+     a=5;
+     b=10; 
+     c=15; 
+     d=20;
+     System.out.println((a>b)&&(c<d));
+     System.out.println((a>b)||(c<d));
+     System.out.println(!(a==b));
+  }
+}
+     
