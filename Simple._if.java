19c07d5f4568d4f23db20828df724b415421b006
sabrina2128 <t717069@gmail.com>
2026-08-06 12:05:27 +0530

    Add files via upload

---
+++ Simple._if.java
@@ -0,0 +1,10 @@
+public class Simple_if {
+   public static void main(String[]args){
+     byte age=17;
+     if(age>=18)
+     {
+       System.out.println("Eligible");
+     }
+     System.out.println("End if");
+   }
+}
