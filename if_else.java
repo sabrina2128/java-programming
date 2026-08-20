8c9446a5205339845960e513d9ee826072073ea7
sabrina2128 <t717069@gmail.com>
2026-08-20 14:52:39 +0900

    Add files via upload

---
+++ if_else.java
@@ -0,0 +1,12 @@
+public class statement{
+   public static void main(String[]args){
+     byte age=11;
+     if(age>=18)
+     {
+       System.out.println("Eligible");
+     }
+     else {
+       System.out.println("Not Eligible");
+     }
+   }
+}
